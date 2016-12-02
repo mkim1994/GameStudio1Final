6207f1ef32a5e2a0839eb7198bf59dc137caa8db
mkim1994 <mkim2@andrew.cmu.edu>
2016-12-02 07:01:22 -0500

    added intro cutscene (basic functionality is there

---
+++ Assets/Scripts/CutsceneHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CutsceneHandler : MonoBehaviour {
+	public string scenename;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+		
+	public void GoToNextScene(){
+
+		SceneManager.LoadScene (scenename);
+	}
+}
