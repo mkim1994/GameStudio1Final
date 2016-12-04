84903d6ea02fed213be95293f58fcb26a29d23d8
mkim1994 <mkim2@andrew.cmu.edu>
2016-12-04 04:58:23 -0500

    added game menu

---
+++ Assets/MenuActions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayGame(){
+		SceneManager.LoadScene ("IntroScene");
+	}
+
+	public void CreditsPage(){
+		print ("credits");
+	}
+
+	public void QuitGame(){
+
+		Application.Quit ();
+	}
+}
