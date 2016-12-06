11a510b84648725e06ad494b68e43cc803fa7195
mkim1994 <mkim2@andrew.cmu.edu>
2016-12-06 13:35:33 -0500

    polish

---
+++ Assets/Scripts/EscapeScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EscapeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//DontDestroyOnLoad (transform.gameObject);
+		//Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (SceneManager.GetActiveScene().name != "Menu" && Input.GetKeyDown (KeyCode.Escape)) {
+			SceneManager.LoadScene ("Menu");
+		}
+	}
+}
